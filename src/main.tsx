d1104a18b866d866179df79e5f309bff8311eff9
yjh <yjv1031@gmail.com>
2026-03-27 21:27:10 +0900

    feat: 버전관리

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import './reset.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
-const BUILD_VERSION_KEY = 'app-build-version'
 const buildId = __APP_BUILD_ID__
-const previousBuildId = localStorage.getItem(BUILD_VERSION_KEY)
 const url = new URL(window.location.href)
 const urlVersion = url.searchParams.get('v')
 
@@ -13,17 +11,6 @@ if (urlVersion !== buildId) {
   url.searchParams.set('v', buildId)
   window.location.replace(url.toString())
 } else {
-  if (previousBuildId !== buildId) {
-    localStorage.setItem(BUILD_VERSION_KEY, buildId)
-
-    if (previousBuildId && 'caches' in window) {
-      caches
-        .keys()
-        .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
-        .finally(() => window.location.reload())
-    }
-  }
-
   createRoot(document.getElementById('root')!).render(
     <BrowserRouter basename="/personal">
       <App />
